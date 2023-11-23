f18a670fc58dafe86bb6c43a193ba66bec10befb
Anthony Ngo <anthony.g.ngo@gmail.com>
2023-11-23 14:06:20 -0800

    moved storage logic to storage class

--- src/index.js
+++ src/index.js
@@ -2,35 +2,6 @@ import Todo from './todo';
 import Project from './project';
 import Storage from './storage';
 
-let tasks = [{ name: 'run' }];
-let projects = [{ name: 'personal' }];
-
-// localStorage.setItem(
-// 	'data',
-// 	JSON.stringify({
-// 		tasks: [{ name: 'Run' }],
-// 		projects: [{ name: 'personal', list: ['id-here'] }],
-// 	})
-// );
-
-const saveToLocalStorage = (tasks, projects) => {
-	localStorage.setItem(
-		'data',
-		JSON.stringify({ tasks: [...tasks], projects: [...projects] })
-	);
-};
-
-const loadFromLocalStorage = () => {
-	const data = localStorage.getItem('data');
-	console.log(data);
-	if (data === null) {
-		return;
-	}
-	const parsedData = JSON.parse(data);
-	tasks = parsedData.tasks;
-	projects = parsedData.projects;
-};
-
 const newTodo = new Todo('Run', 'Run a lot', '2023-12-11', 'high');
 
 newTodo.changeDescription('Gotta go fast');
--- src/storage.js
+++ src/storage.js
@@ -1,3 +1,24 @@
-class Storage {}
+class Storage {
+	static tasks = [{ name: 'run' }];
+	static projects = [{ name: 'personal' }];
+
+	static saveToLocalStorage = (tasks, projects) => {
+		localStorage.setItem(
+			'data',
+			JSON.stringify({ tasks: [...tasks], projects: [...projects] })
+		);
+	};
+
+	static loadFromLocalStorage = () => {
+		const data = localStorage.getItem('data');
+		console.log(data);
+		if (data === null) {
+			return;
+		}
+		const parsedData = JSON.parse(data);
+		tasks = parsedData.tasks;
+		projects = parsedData.projects;
+	};
+}
 
 export default Storage;
