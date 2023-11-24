64efcda3b3ad0b43e5f7ed484d61db5e4ee12c31
Anthony Ngo <anthony.g.ngo@gmail.com>
2023-11-23 19:00:17 -0800

    clean up testing functions

--- src/index.js
+++ src/index.js
@@ -16,13 +16,3 @@ const createNewProject = (name) => {
 	Storage.projects.push(project);
 	Storage.saveToLocalStorage();
 };
-
-const task = createNewTask('Run', 'Run a lot', '2022-12-11', 'high');
-const taskTwo = createNewTask('swimming', 'swim some', '2022-12-12', 'low');
-
-taskTwo.changeName('Go Diving!');
-task.changeName('walk');
-Storage.saveToLocalStorage();
-// createNewProject('personal');
-// task.name = 'Walk';
-console.log(localStorage);
