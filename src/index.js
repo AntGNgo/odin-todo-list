82191089fad8ead3d4ef4eebd776980248336478
Anthony Ngo <anthony.g.ngo@gmail.com>
2023-11-23 17:48:01 -0800

    removed update function as arrays are passed by ref

--- src/index.js
+++ src/index.js
@@ -14,14 +14,15 @@ const createNewTask = (name, description, date, priority) => {
 const createNewProject = (name) => {
 	const project = new Project(name);
 	Storage.projects.push(project);
+	Storage.saveToLocalStorage();
 };
 
 const task = createNewTask('Run', 'Run a lot', '2022-12-11', 'high');
-console.log(task);
-console.log(localStorage);
-task.name = 'Walk';
+const taskTwo = createNewTask('swimming', 'swim some', '2022-12-12', 'low');
 
-Storage.findAndReplaceTask(task);
-Storage.saveToLocalStorage(task);
-
+taskTwo.changeName('Go Diving!');
+task.changeName('walk');
+Storage.saveToLocalStorage();
+// createNewProject('personal');
+// task.name = 'Walk';
 console.log(localStorage);
--- src/project.js
+++ src/project.js
@@ -1,3 +1,11 @@
-class Project {}
+import { v4 as uuidv4 } from 'uuid';
+
+class Project {
+	constructor(name) {
+		this.name = name;
+		this.list = [];
+		this.id = uuidv4();
+	}
+}
 
 export default Project;
